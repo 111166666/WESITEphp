04d94775ed0223e77c4ef48aff5b7e10b484093d
111166666 <limaclau777@gmail.com>
2022-08-23 10:56:44 +0100

    I chanhe the html file

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Project 01</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/reset.css">
+  </head>
+ <body>
+
+    <nav>
+        <div class="""wrapper">
+        <a href="index.php">img src="img/logo-white.png" alt="Blogs logo"</a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="discover.php">About Us</a></li>
+            <li><a href="blog.php">Find Blogs</a></li>
+            <li><a href="signup.php">sign up</a></li>
+            <li><a href="login.php">log in</a></li>
+        </ul>
+       </div>
+    </nav>
+
+  <div class="wrapper">
+
+      <section class="index-intro">
+          <h1>This is an Introduction</h1>
+      <p>Here is a inportant paragraph that explains</p>
+      </section>
+
+      <section class="index-castegories">
+          <h2>Some Basic Categories</h2>
+           <div class="index-categories-list">
+          <div>
+              <h3>Fun Stuff></h3>
+          </div>
+          <div>
+            <h3>Serious Stuff></h3>
+          </div>
+          <div>
+              <h3>Exciting Stuff></h3>
+          </div>
+          <div>
+          <h3>boring Stuff></h3>
+      </div>
+           </div>
+ </section>
+
+ </div>
+</body>
+</html>
+
+<script src="js/>script.js"></script>
